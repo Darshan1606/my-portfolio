4d4c1ec6eaed1618dac75a70953b01ba80bdf6fb
Darshan1606 <darshantarsariya17@gmail.com>
2023-04-09 11:30:36 +0530

    date update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-center p-8 bg-violet-700 text-white">
       <div>darshantarsariya17@gmail.com</div>
       <div>
-        <div>Copyright @ 2023 | &nbsp; Darshan Tarsariya</div>
+        <div>
+          Copyright @ {new Date().getFullYear()} | &nbsp; Darshan Tarsariya
+        </div>
       </div>
       <div>
         <a href="#contactme">Contact Me</a>
